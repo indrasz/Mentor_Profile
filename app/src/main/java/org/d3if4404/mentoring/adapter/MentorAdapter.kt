cf954842776a06f3d85c34e895b0904509458afd
wahyuindra <wahyuindra712@gmail.com>
2022-03-31 09:28:59 +0700

    add home views

---
+++ app/src/main/java/org/d3if4404/mentoring/adapter/MentorAdapter.kt
@@ -0,0 +1,4 @@
+package org.d3if4404.mentoring.adapter
+
+class MentorAdapter {
+}
--- app/src/main/java/org/d3if4404/mentoring/views/MainActivity.kt
+++ app/src/main/java/org/d3if4404/mentoring/views/MainActivity.kt
@@ -2,11 +2,14 @@ package org.d3if4404.mentoring.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import org.d3if4404.mentoring.R
+import org.d3if4404.mentoring.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
 }
